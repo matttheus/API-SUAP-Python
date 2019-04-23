a295b47ebc67da2435f7c83297f4c9043ee16d90
matttheus <30329820+matttheus@users.noreply.github.com>
2019-04-22 22:55:32 -0300

    Change the names of the variables to english names

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 import requests
 
-# Obtendo o token do usuário.
+# Getting the user token
 url = 'https://suap.ifrn.edu.br/api/v2/autenticacao/token/'
 
-#username e o password são os seus dados utilizads para acessar o SUAP
-dados_usuario = {
+#username and0 password are parameters of requisition
+user_data = {
   'username': '',
   'password': ''
 }
 
-requisicao = requests.post(url, data=dados_usuario)
-if requisicao.status_code == requests.codes.ok:
-  token_autenticacao = requisicao.json().get('token')
-  print ('\n--- Token de Autenticação:\n {} \n\n'.format(token_autenticacao))
+requisition = requests.post(url, data=user_data)
+if requisition.status_code == requests.codes.ok:
+  token_authentication = requisition.json().get('token')
+  print ('\n--- Authentication token:\n {} \n\n'.format(token_authentication))
 
-# Obtendo os dados do usuário.
+# Getting the user data
 url = 'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/'
+
 headers = {
-  'Authorization':'JWT {}'.format(token_autenticacao)
+  'Authorization':'JWT {}'.format(token_authentication)
 }
-requisicao = requests.get(url, headers=headers)
-if requisicao.status_code == requests.codes.ok:
-  retorno_json = requisicao.json()
-  print ('--- Dados do Usuário Logado:\n{}\n\n'.format(retorno_json))
+
+requisition = requests.get(url, headers=headers)
+if requisition.status_code == requests.codes.ok:
+  json_return = requisition.json()
+  print ('--- User data:\n{}\n\n'.format(json_return))
